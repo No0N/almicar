9f587b6315f615ebcd404593356e5d2000302787
No0N <noon@live.ru>
2018-12-30 17:11:18 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$DB_server = 'localhost';
-$DB_user = 'home40540_car';
-$DB_pass = 'D2313d';
-$DB_base = 'home40540_car';
+$DB_server = '*';
+$DB_user = '*';
+$DB_pass = '*';
+$DB_base = '*';
 $file_name = 'file/'.date(Y).date(m).date(d).time().'.csv';
 
 ?>
